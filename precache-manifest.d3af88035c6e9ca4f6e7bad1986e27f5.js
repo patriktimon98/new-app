0674dbafdcb52bfbb75af97a4231923fde2f5e34
patriktimon98 <patrik.ivanov.1990@mail.ru>
2019-07-17 01:52:48 +1000

    Updates

---
+++ precache-manifest.d3af88035c6e9ca4f6e7bad1986e27f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0789cb1186d6e8020b6c16361d7b312",
+    "url": "/new-app/index.html"
+  },
+  {
+    "revision": "34eadd3c7704fa1fa41b",
+    "url": "/new-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/new-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "34eadd3c7704fa1fa41b",
+    "url": "/new-app/static/js/main.daa5ae9f.chunk.js"
+  },
+  {
+    "revision": "b0e179c86933ae8a6ac7",
+    "url": "/new-app/static/js/runtime~main.9d331d9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
